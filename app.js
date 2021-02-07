5a0263430c93ea47d3e1b65168a3c9a675b54768
Chris Mosey <chris.c.mosey@gmail.com>
2021-02-06 21:26:29 -0500

    Updated to express, added more coolness

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const http = require('http');
+const express = require("express");
+const path = require("path");
 
-const code = "<html><h1><h1>(•_•) ( •_•)>⌐■-■ (⌐■_■) (▀̿Ĺ̯▀̿ ̿)</h1></html>";
+const app = express();
 
-const server = http.createServer((req, res) => {
-	res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-	res.end(code);
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname + "/index.html"));
 });
-server.listen(80);
+
+app.listen(80);
 console.log("server listening at localhost:80");
